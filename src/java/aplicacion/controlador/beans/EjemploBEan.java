d202aed7bfdb4e2fa08c21289247b4d9bb91b731
Ariel Vega <Ariel Vega@LAPTOP-S9GJ9J8F>
2019-04-27 12:34:36 -0300

    Estructura de app web finalizada

---
+++ src/java/aplicacion/controlador/beans/EjemploBEan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.controlador.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Ariel Vega
+ */
+@ManagedBean
+@ViewScoped
+public class EjemploBEan {
+
+    /**
+     * Creates a new instance of EjemploBEan
+     */
+    public EjemploBEan() {
+    }
+    
+}
---
+++ src/java/aplicacion/controlador/beans/forms/EjemploFormBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.controlador.beans.forms;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Ariel Vega
+ */
+@ManagedBean
+@ViewScoped
+public class EjemploFormBean {
+
+    /**
+     * Creates a new instance of EjemploFormBean
+     */
+    public EjemploFormBean() {
+    }
+    
+}
